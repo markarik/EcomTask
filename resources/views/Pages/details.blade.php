00d5ed41aa755758648b0b3573fd7617cd0ed688
markarik <kariukimarkg@gmail.com>
2019-06-07 21:34:27 +0300

    another

--- resources/views/Pages/details.blade.php
+++ resources/views/Pages/details.blade.php
@@ -22,14 +22,12 @@
         {{--main image --}}
                 <div class="col-sm-5">
                         <div class="cards" style="width: 20rem;">
-                                <a href="#"><img src="{{asset('images/2.jpg')}}" class="image" alt="Imac"></a> 
-
+                                <a href="#"><img src="{{asset('images/2.jpg')}}" class="image" alt="Imac"></a>
                                 <button class="btn btn-secondary"><i class="fas fa-heart"></i>Watch list</button>
                                 <button class="btn btn-success"><i class="fas fa-cart-plus"></i>Add To Cart</button>                
                         </div>
                 </div>
                 {{--other details--}}
-
                 <div class="col-sm-2">
                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</p>      
                 </div>  
@@ -41,20 +39,20 @@
         <div class="row">
                 <div class="col-sm-3">
                         <div>
-                                <h5>Number of Ratings</h5>
+                           <h5>Number of Ratings</h5>
                         </div>
                         <div>
-                                <h5>Comments</h5>
+                           <h5>Comments</h5>
                         </div>
                 </div>
                 <div class="col-sm-3">
-                        <h5>Reviews</h5>
+                  <h5>Reviews</h5>
                 </div>
                 <div class="col-sm-3">
-                        <h5>Description</h5>
+                    <h5>Description</h5>
                 </div>
                 <div class="col-sm-3">
-                        <h5>Return Policy</h5>
+                   <h5>Return Policy</h5>
                 </div>
         </div>
 </div>
