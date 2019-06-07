695464e46b33eca4b449ce31840c5bfb93c18e6a
markarik <kariukimarkg@gmail.com>
2019-06-07 17:20:23 +0300

    again

--- resources/views/Pages/details.blade.php
+++ resources/views/Pages/details.blade.php
@@ -35,7 +35,7 @@
                 </div>  
                         {{-- Ratings --}}  
                 <div class="col-sm-2">
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</p>
+                        <p> fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</p>
                 </div>
         </div>
         <div class="row">
